09f2076513e75c5e7211972dac0a8252a6d27248
Zhang LiangZhi <3717591@qq.com>
2017-01-01 01:32:07 +0800

    go

--- RandomNum/ViewController.swift
+++ RandomNum/ViewController.swift
@@ -7,6 +7,13 @@
 //
 
 import UIKit
+import CoreData
+
+let appDelegate = UIApplication.shared.delegate as! AppDelegate
+let context = appDelegate.persistentContainer.viewContext
+var arrRandomNum:[RandomData] = []
+var arrGlobalSet:[CurGlobalSet] = []
+var nowGlobalSet:CurGlobalSet?
 
 class ViewController: UIViewController {
 
@@ -16,6 +23,11 @@ class ViewController: UIViewController {
         
 
         self.title = NSLocalizedString("Setting", comment: "")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getCoreData()
+        firstOpenAPP()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,6 +43,46 @@ class ViewController: UIViewController {
         }
         
     }
+
+    func getCoreData() -> Void {
+        arrRandomNum = []
+        arrGlobalSet = []
+        
+        do {
+            arrRandomNum = try context.fetch(RandomData.fetchRequest())
+        }catch {
+            print("RandomData coreData error")
+        }
+        
+        do {
+            arrGlobalSet = try context.fetch(CurGlobalSet.fetchRequest())
+        }catch {
+            print("Setting coreData error")
+        }
+
+        if arrGlobalSet.count > 0 {
+            nowGlobalSet = arrGlobalSet[0]
+        }
+        print(nowGlobalSet)
+    }
+    
+    // 第一次打开app，加入测试数据
+    func firstOpenAPP() -> Void {
+        // 初始化
+        if arrGlobalSet.count > 0 {
+            return
+        }
+        
+        let oneGlobalSet = NSEntityDescription.insertNewObject(forEntityName: "CurGlobalSet", into: context) as! CurGlobalSet
+        oneGlobalSet.title = NSLocalizedString("Set Title", comment: "")
+        oneGlobalSet.endID = 3
+        oneGlobalSet.startID = 1
+        oneGlobalSet.repeatNum = 0
+        context.insert(oneGlobalSet)
+        appDelegate.saveContext()
+        getCoreData()
+    }
+    
 
 }
 
